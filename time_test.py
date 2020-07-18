92af9391a99a77e4ed76311cfcf1b49d65ad20ad
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-07-18 11:21:26 +0500

    Add decorator as a class

--- time_test.py
+++ time_test.py
@@ -1,10 +1,10 @@
 
 import time
 
+# decorator as a function
 def time_this(num_runs):
     def wrapper(func):
         avg_time = 0
-        # print(num_runs)
         for i in range(num_runs):
             print(i)
             t0 = time.time()
@@ -17,10 +17,27 @@ def time_this(num_runs):
         return func
     return wrapper
 
+# decorator as a class
+class TimeThis:
+    def __init__(self, num_runs):
+        self.num_runs = num_runs
 
-@time_this(num_runs=10)
+    def __call__(self, func):
+        avg_time = 0
+        for i in range(self.num_runs):
+            print(i)
+            t0 = time.time()
+            func()
+            t1 = time.time()
+            avg_time += (t1 - t0)
+
+        avg_time /= self.num_runs
+        print(f"avg_time: {avg_time}")        
+        return func
+
+# @time_this(num_runs=10)
+@TimeThis(num_runs=10)
 def f():
-    # print("f")
     for i in range(10_000_000):
         pass
 
